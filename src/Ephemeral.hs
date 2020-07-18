011a19ce4381ff0d0e8b9c7d386735079b234569
Tony Day <tonyday567@gmail.com>
2020-07-18 18:09:27 +1000

    simplified

--- src/Ephemeral.hs
+++ src/Ephemeral.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE RankNTypes #-}
@@ -10,8 +15,7 @@ Functional machinery, rapid learning.
 
 module Ephemeral where
 
-import NumHask.Prelude
-import Data.Functor.Contravariant
+import NumHask.Prelude hiding (evaluate)
 import Data.Profunctor
 
 {- |
@@ -58,3 +62,56 @@ improve progress es task =
     task' = change (learn progress) es task
     p = measure task <$> set es
     p' = measure task' <$> set es
+
+{-
+
+If we take a linear regression, with parameters of alpha and betas unified as (a:bs), gives a carrier experience set of ([a], a). We take an [a] (the dependent variables) and produce an a, our guess as to the independent variable. We then take the second element of the tuple and know it is the underlying true answer to our guess. The difference between our guess and the correct answer is our measure, with the closer to zero the better.
+
+-}
+
+newtype Params a = Params { act :: [a] } deriving (Eq, Show, Generic)
+
+error :: (Ring a) => Params a -> ([a], a) -> a
+error (Params bs) (es, y) = sum (zipWith (+) bs (one:es)) - y
+
+errors :: (Functor f, Ring a) => Params a -> Experience f ([a],a) -> f a
+errors p (Experience es) = error p <$> es
+
+newtype Population f a = Population { individuals :: f (Params a) } deriving (Generic)
+
+-- | The value of a population, for each experience.
+-- value :: Population f a -> Experience g ([a], a) -> g a
+value :: (Traversable p, Traversable e, Applicative e, Ring a) =>
+  Population p a ->
+  Experience e ([a], a) ->
+  (e (p a) -> p a) ->
+  p a
+value (Population ps) (Experience es) f = f $ traverse (\p -> error p <$> es) ps
+
+-- | A heuristic is any approach to problem solving or *self-discovery* that employs a practical method, not guaranteed to be optimal, perfect, or rational.
+--
+-- populations evolve according to heuristics.
+--
+-- They are natural transformations, with a carrier phenotype.
+newtype Heuristic f g a = Heuristic { evolve :: Population f a -> Population g a }
+
+
+{-
+-- cofunctor, like an average
+type Neighbourhood = Heuristic Identity
+
+-- mutation is isomorphic to (->).
+type Mutation = Heuristic Identity Identity
+
+-- crossover is foldable
+type Crossover f = Heuristic f Identity
+
+-- an individua can also be a carrier of the algebra
+type Individual a = Population Identity a
+
+
+data PredictionType a c =
+  Regression {fit :: (Ord a, Num a) => Params a} |
+  Classification { enumerate :: (Eq c) => Params c}
+
+-}
