40ab24007dc6a181ca7f4f28cc8766b04d3aaff3
Tony Day <tonyday567@gmail.com>
2020-08-17 10:34:44 +1000

    shekel chart

--- src/Ephemeral/Charts.hs
+++ src/Ephemeral/Charts.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE OverloadedLabels #-}
@@ -9,60 +11,17 @@ module Ephemeral.Charts where
 import NumHask.Prelude hiding (zero,one)
 import qualified NumHask.Prelude as P
 import Chart hiding (norm)
+import Chart.Serve
 import Ephemeral.Shekel
 import Numeric.Backprop
 import Lens.Micro
 import System.Random.MWC
 import Control.Monad.Primitive
+import Box
+import Control.Monad.Conc.Class as C
+import qualified Data.List as List
 
-scratch :: ([Chart Double], [Hud Double]) -> IO ()
-scratch (cs, hs) = writeFile "other/scratch.svg" $ renderHudOptionsChart defaultSvgOptions defaultHudOptions hs cs
-
-shekel' :: Int -> [(Rect Double, Double)]
-shekel' grain =
-  (\p@(Point x y) ->
-     (widen (Point (P.one / fromIntegral grain) (P.one / fromIntegral grain))
-      (toRect (PointXY (Point x y))), (evalBP (shekp . norm') p))) <$>
-  grid MidPos (P.one :: Rect Double) (Point grain grain)
-
-shekel'' :: Int -> Rect Double -> [(Point Double, Double)]
-shekel'' grain r =
-  (\p@(Point x y) ->
-     ((Point x y), evalBP (shekp . norm') p)) <$>
-  grid MidPos r (Point grain grain)
-
--- > scratch $ shekelc 20 one
-shekelc :: Int -> Rect Double -> ([Chart Double], [Hud Double])
-shekelc grain r =
-  pixelfl (evalBP (shekp . norm')) (PixelOptions defaultPixelStyle (Point grain grain) r)
-  (defaultPixelLegendOptions "shekel")
-
-norm_ :: (FromRational a, Multiplicative a, Additive a) => Point a -> Point a
-norm_ (Point x y) = Point (10.0 * (x + 0.5)) (10.0 * (y + 0.5))
-
-norm' :: (Backprop a, Distributive a, Subtractive a, Reifies s W, FromRational a) => BVar s (Point a) -> BVar s (Point a)
-norm' p =
-  p2t p &
-  _1 %~~ (\x -> (constVar 10.0 * (x + constVar 0.5))) &
-  _2 %~~ (\x -> (constVar 10.0 * (x + constVar 0.5))) &
-  t2p
-
--- > scratch $ first (<> gradLine 0.0005 (gradShekel 10 P.one)) $ shekelc 10 P.one
-gradShekel :: Int -> Rect Double -> [(Point Double, Point Double)]
-gradShekel grain r =
-  (\p@(Point x y) ->
-     ((Point x y), gradBP (shekp . norm') p)) <$>
-  grid MidPos r (Point grain grain)
-
-gradLine :: Double -> [(Point Double, Point Double)] -> [Chart Double]
-gradLine s ps = (\(p,p') -> Chart (LineA (defaultLineStyle & #width .~ 0.005 & #color .~ black)) [PointXY p, PointXY (p + fmap (s *) p')]) <$> ps
-
-distancep :: ExpField a => Point a -> a
-distancep (Point x y) = sqrt (x ^ (2::Int) + y ^ (2::Int))
-
-normp :: ExpField a => Point a -> Point a
-normp p = fmap (/distancep p) p
-
+-- | Generate a point within a rectangle.
 uniformP :: (PrimMonad m, Variate a) => Gen (PrimState m) -> Rect a -> m (Point a)
 uniformP g (Rect x z y w) = Point <$> (uniformR (x,z) g) <*> (uniformR (y,w) g)
 
@@ -75,4 +34,71 @@ rvp n r = do
   g <- create
   replicateM n (uniformP g r)
 
+-- | testing
+scratch :: ([Chart Double], [Hud Double]) -> IO ()
+scratch (cs, hs) = writeFile "other/scratch.svg" $ renderHudOptionsChart defaultSvgOptions defaultHudOptions hs cs
+
+-- | chart a surface
+surface :: Int -> Rect Double -> (forall s. (Reifies s W) => BVar s (Point Double) -> BVar s Double) -> (HudOptions, [Hud Double], [Chart Double])
+surface grain r f = (\(cs,hs) -> (defaultHudOptions,hs,cs)) $
+  pixelfl (evalBP f) (PixelOptions defaultPixelStyle (Point grain grain) r)
+  (defaultPixelLegendOptions mempty)
+
+-- | climb a gradient by taking a fixed step, rejecting worse points.
+climb :: Double -> (forall s. (Reifies s W) => BVar s (Point Double) -> BVar s Double) -> Point Double -> Point Double
+climb r f p = bool p p' (v' > fst bp)
+  where
+    bp :: (Double, Point Double)
+    bp = backprop f p
+    p' :: (Point Double)
+    p' = p + fmap (r*) (normp (snd bp))
+    v' = evalBP f p'
+
+-- | climbing requires a distance metric
+distancep :: ExpField a => Point a -> a
+distancep (Point x y) = sqrt (x ^ (2::Int) + y ^ (2::Int))
+
+normp :: ExpField a => Point a -> Point a
+normp p = fmap (/distancep p) p
+
+-- | Background pixel chart for the surface.
+-- > scratch $ shekelc 20 one
+shekelc :: Int -> Rect Double -> (HudOptions, [Hud Double], [Chart Double])
+shekelc grain r = surface grain r (shekp . norm')
+
+-- | chart latest population
+next :: (Monad m) => Int -> Double -> (forall s. (Reifies s W) => BVar s (Point Double) -> BVar s Double) -> StateT [Point Double] m (HudOptions, [Hud Double], [Chart Double])
+next g r f = do
+  ps <- get
+  let ps' = climb r f <$> ps
+  put ps'
+  pure (ho, hs, cs <> chartSnail ps')
+    where
+      chartSnail ps = zipWith (\p c -> Chart (GlyphA (defaultGlyphStyle & #color .~ c & #size .~ 0.02)) [PointXY p]) ps (List.cycle palette1)
+      (ho, hs, cs) = shekelc g P.one
+
+nextE :: (Monad m) => Int -> Double -> (forall s. (Reifies s W) => BVar s (Point Double) -> BVar s Double) -> Emitter (StateT [Point Double] m) (HudOptions, [Hud Double], [Chart Double])
+nextE g gap f = Emitter $ do
+  n <- next g gap f
+  pure $ Just n
+
+emitNextChart :: (MonadConc m) => Int -> Int -> Double -> (forall s. (Reifies s W) => BVar s (Point Double) -> BVar s Double) -> [Point Double] ->
+  Cont m (Emitter m (HudOptions, [Hud Double], [Chart Double]))
+emitNextChart n g gap f ps0 = emitQ (\c -> flip evalStateT ps0 (glueN n (hoist lift c) (nextE g gap f)))
+
+emitChart :: Int -> Int -> Double -> (forall s. (Reifies s W) => BVar s (Point Double) -> BVar s Double) -> SConfig -> IO (Cont IO (Emitter IO (HudOptions, [Hud Double], [Chart Double])))
+emitChart n g gap f cfg = do
+  ps0 <- rvp n P.one
+  pure $ delaySecs (1 / cfg ^. #framerate) <$> (emitNextChart (totalFrames cfg) g gap f ps0)
+
+toSvgText :: (HudOptions, [Hud Double], [Chart Double]) -> Text
+toSvgText (ho, hs, cs) = renderHudOptionsChart defaultSvgOptions ho hs cs
+
+emitText :: Int -> Int -> Double -> (forall s. (Reifies s W) => BVar s (Point Double) -> BVar s Double) -> SConfig -> IO (Cont IO (Emitter IO Text))
+emitText n g gap f cfg = fmap (fmap toSvgText) <$> emitChart n g gap f cfg
+
+runc :: IO ()
+runc = do
+  e <- emitChart 200 20 0.02 (shekp . norm') (defaultSConfig & #framerate .~ 2 & #runtime .~ 60)
+  serveCharts <$.> e
 
--- src/Ephemeral/Shekel.hs
+++ src/Ephemeral/Shekel.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE MonoLocalBinds #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE RebindableSyntax #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 
 -- | https://en.wikipedia.org/wiki/Shekel_function
 module Ephemeral.Shekel where
 
+import Chart
 import NumHask.Prelude as P
 import Numeric.Backprop
 import NumHask.Backprop ()
-import NumHask.Space hiding (toPoint)
+import Lens.Micro
 
 -- shekel function
 sa :: [[Double]]
@@ -94,7 +97,7 @@ sdiff xs = collectVar $
   fmap sum $ fmap (fmap (^(2::Int))) $ (\r -> zipWith (-) (sequenceVar xs) r) <$> (sequenceVar . constVar <$> sa)
 
 sinv :: (Reifies s W) => BVar s [Double] -> BVar s Double
-sinv xs = sum $ fmap (1/) (zipWith (+) (sequenceVar xs) (constVar <$> sc))
+sinv xs = sum $ fmap (P.one/) (zipWith (+) (sequenceVar xs) (constVar <$> sc))
 
 shek :: (Reifies s W) => BVar s [Double] -> BVar s Double
 shek = sinv . sdiff
@@ -134,4 +137,29 @@ p2t =
   isoVar
   (\(Point x y) -> (x,y))
   (\(x,y) -> Point x y)
+
+
+-- * shekel specific
+
+norm_ :: (FromRational a, Multiplicative a, Additive a) => Point a -> Point a
+norm_ (Point x y) = Point (10.0 * (x + 0.5)) (10.0 * (y + 0.5))
+
+-- | Normalise the shekel function to the unit rectangle.
+norm' :: (Backprop a, Distributive a, Subtractive a, Reifies s W, FromRational a) => BVar s (Point a) -> BVar s (Point a)
+norm' p =
+  p2t p &
+  _1 %~~ (\x -> (constVar 10.0 * (x + constVar 0.5))) &
+  _2 %~~ (\x -> (constVar 10.0 * (x + constVar 0.5))) &
+  t2p
+
+
+-- > scratch $ first (<> gradLine 0.0005 (gradShekel 10 P.one)) $ shekelc 10 P.one
+gradShekel :: Int -> Rect Double -> [(Point Double, Point Double)]
+gradShekel grain r =
+  (\p@(Point x y) ->
+     ((Point x y), gradBP (shekp . norm') p)) <$>
+  grid MidPos r (Point grain grain)
+
+gradLine :: Double -> [(Point Double, Point Double)] -> [Chart Double]
+gradLine s ps = (\(p,p') -> Chart (LineA (defaultLineStyle & #width .~ 0.005 & #color .~ black)) [PointXY p, PointXY (p + fmap (s *) p')]) <$> ps
 
